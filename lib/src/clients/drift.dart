4ba0beeaec7f955ac8bf557f9fbb539902274f83
우주미남도원 <flxh4894@gmail.com>
2024-02-20 00:50:02 +0900

    drift 관련 안드로이드 임시폴더위치 추가

--- lib/src/clients/drift.dart
+++ lib/src/clients/drift.dart
@@ -5,6 +5,7 @@ import 'package:drift/native.dart';
 import 'package:easy_cart/database/db.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
+import 'package:sqlite3/sqlite3.dart';
 import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
 
 part 'drift.g.dart';
@@ -46,10 +47,10 @@ LazyDatabase _openConnection() {
 
     // // Make sqlite3 pick a more suitable location for temporary files - the
     // // one from the system may be inaccessible due to sandboxing.
-    // final cachebase = (await getTemporaryDirectory()).path;
     // // We can't access /tmp on Android, which sqlite3 would try by default.
     // // Explicitly tell it about the correct temporary directory.
-    // sqlite3.tempDirectory = cachebase;
+    final cachebase = (await getTemporaryDirectory()).path;
+    sqlite3.tempDirectory = cachebase;
 
     return NativeDatabase.createInBackground(file);
   });
